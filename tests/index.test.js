66f78edfe6029fcab626be75528392ee56e3b154
hinkleydev <azz@hinkley.dev>
2024-11-04 15:29:55 +0000

    Basic GET tests done

---
+++ tests/index.test.js
@@ -0,0 +1,41 @@
+const { it, describe, expect } = require("@jest/globals");
+const Restaurant = require("../models");
+const baseUrl = "http://localhost:3000/restaurants";
+
+describe("GET /restaurants", function() {
+    it("responds 200", async function() {
+        const response = await fetch(baseUrl);
+        expect(response.status).toBe(200);
+    })
+
+    it("returns an array of restaurants", async function() {
+        const response = await fetch(baseUrl)
+        const parsed = await response.json();
+        for(line of parsed) {
+            // Flick through each object and check it has the fields
+            expect(line).toHaveProperty("name");
+            expect(line).toHaveProperty("cuisine");
+            expect(line).toHaveProperty("location");
+        }
+    })
+
+    it("returns the correct amount of restaurants", async function() {
+        // Get the right number to check for
+        const restaurants = await Restaurant.findAll();
+        const correctAmount = restaurants.length;
+        const response = await fetch(baseUrl)
+        const parsed = await response.json();
+        expect(parsed.length).toBe(correctAmount);
+    })
+
+    it("returns the correct restaurant data", async function() {
+        // Example data
+        const restaurants = await Restaurant.findAll();
+        const response = await fetch(baseUrl);
+        const parsed = await response.json();
+        for(index in restaurants) {
+            // Object comparison
+            expect(parsed[index]).toBe(parsed[index]);
+        }
+    })
+})
