2d908e11f906fed3ad348ef7f2c7a1423dc05d29
hinkleydev <azz@hinkley.dev>
2024-11-04 13:35:10 +0000

    Refactored to use routes folder

---
+++ routes/restaurant.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const restaurantRoute = express.Router();
+const Restaurant = require("../models/index")
+
+// Middleware
+restaurantRoute.use(express.json());
+restaurantRoute.use(express.urlencoded());
+
+// All restaurants
+restaurantRoute.get("/restaurants", async function(req, res) {
+    const restaurants = await Restaurant.findAll();
+    const result = JSON.stringify(restaurants)
+    res.send(result);
+})
+
+// Specific restaurants
+restaurantRoute.get("/restaurants/:id", async function(req, res) {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    if (restaurant == null) {
+        res.send("Restaurant not found");
+        return;
+    }
+    const result = JSON.stringify(restaurant);
+    res.send(result);
+})
+
+// Add new restaurant
+restaurantRoute.put("/restaurants", async function(req, res) {
+    const name = req.body.name;
+    const location = req.body.location;
+    const cuisine = req.body.cuisine;
+    const restaurant = await Restaurant.create({name, location, cuisine});
+    res.send(restaurant);
+})
+
+// Change restaurant
+restaurantRoute.post("/restaurants/:id", async function(req, res) {
+    const name = req.body.name;
+    const location = req.body.location;
+    const cuisine = req.body.cuisine;
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    if (restaurant == null) {
+        res.send("Restaurant not found");
+        return;
+    }
+    await restaurant.update({name, location, cuisine});
+    res.send(restaurant);
+})
+
+// Delete restaurant
+restaurantRoute.delete("/restaurants/:id", async function(req, res) {
+    let restaurant = await Restaurant.findByPk(req.params.id);
+    if (restaurant == null) {
+        res.send("Restaurant not found");
+        return;
+    }
+    await restaurant.destroy();
+    res.send("Deleted");
+})
+
+module.exports = restaurantRoute;
--- src/app.js
+++ src/app.js
@@ -1,64 +1,11 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
-const db = require("../db/connection");
-const { EmptyResultError } = require("sequelize");
+const restaurantRoute = require("../routes/restaurant");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded());
 
-// All restaurants
-app.get("/restaurants", async function(req, res) {
-    const restaurants = await Restaurant.findAll();
-    const result = JSON.stringify(restaurants)
-    res.send(result);
-})
-
-// Specific restaurants
-app.get("/restaurants/:id", async function(req, res) {
-    const restaurant = await Restaurant.findByPk(req.params.id);
-    if (restaurant == null) {
-        res.send("Restaurant not found");
-        return;
-    }
-    const result = JSON.stringify(restaurant);
-    res.send(result);
-})
-
-// Add new restaurant
-app.put("/restaurants", async function(req, res) {
-    const name = req.body.name;
-    const location = req.body.location;
-    const cuisine = req.body.cuisine;
-    const restaurant = await Restaurant.create({name, location, cuisine});
-    res.send(restaurant);
-})
-
-// Change restaurant
-app.post("/restaurants/:id", async function(req, res) {
-    const name = req.body.name;
-    const location = req.body.location;
-    const cuisine = req.body.cuisine;
-    const restaurant = await Restaurant.findByPk(req.params.id);
-    if (restaurant == null) {
-        res.send("Restaurant not found");
-        return;
-    }
-    await restaurant.update({name, location, cuisine});
-    res.send(restaurant);
-})
-
-// Delete restaurant
-app.delete("/restaurants/:id", async function(req, res) {
-    let restaurant = await Restaurant.findByPk(req.params.id);
-    if (restaurant == null) {
-        res.send("Restaurant not found");
-        return;
-    }
-    await restaurant.destroy();
-    res.send("Deleted");
-})
-
+app.use("/", restaurantRoute);
 
 module.exports = app;
