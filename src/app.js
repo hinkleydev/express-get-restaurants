8d478445ba1efa9dc428d4841ceaa3c6a20f8b50
hinkleydev <azz@hinkley.dev>
2024-11-04 10:37:57 +0000

    Part one created

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
+app.get("/restaurants", async function(req, res) {
+    const restaurants = await Restaurant.findAll();
+    const result = JSON.stringify(restaurants)
+    res.send(result);
+})
 
 
 
