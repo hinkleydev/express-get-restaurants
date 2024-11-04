33e1adb76c25c7b435a4a16d30ba80293a1bc8ee
hinkleydev <azz@hinkley.dev>
2024-11-04 11:27:03 +0000

    Added ability to create restaurants

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
+const { EmptyResultError } = require("sequelize");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded());
 
 // All restaurants
 app.get("/restaurants", async function(req, res) {
@@ -15,6 +20,16 @@ app.get("/restaurants/:id", async function(req, res) {
     const restaraunt = await Restaurant.findByPk(req.params.id);
     const result = JSON.stringify(restaraunt);
     res.send(result);
+})
+
+// Add new restaurant
+app.post("/restaurants", async function(req, res) {
+    const name = req.body.name;
+    const location = req.body.location;
+    const cuisine = req.body.cuisine;
+    const restaurant = Restaurant.create({name, location, cuisine});
+    const result = JSON.stringify(restaurant);
+    res.send("Added!");
 })
 
 
